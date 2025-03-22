1b25a6bb0ba90ca05eee7a3ea1a7791c71916e33
Pavel Jiranek <pavel.jiranek@gmail.com>
2025-03-22 16:30:20 +0100

    Initial commit

---
+++ client.py
@@ -0,0 +1,25 @@
+import socket
+import struct
+import argparse
+
+parser = argparse.ArgumentParser(description='UDP Client that receives random numbers')
+parser.add_argument('--host', default="127.0.0.1",
+                   help='Host IP address (default: 127.0.0.1)')
+parser.add_argument('--port', type=int, default=1234,
+                   help='Port number (default: 1234)')
+
+args = parser.parse_args()
+
+host = args.host
+port = args.port
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((host, port))
+
+    while True:
+        data = s.recv(1024)
+        if not data:
+            break
+
+        num1, num2 = struct.unpack('dd', data)
+        print(f"Received: {num1}, {num2}")
---
+++ server.py
@@ -0,0 +1,36 @@
+import socket
+import time
+import random
+import struct
+import argparse
+
+parser = argparse.ArgumentParser(description='UDP Server that sends random numbers')
+parser.add_argument('--host', default="0.0.0.0", help='Host IP address (default: 0.0.0.0)')
+parser.add_argument('--port', type=int, default=1234, help='Port number (default: 1234)')
+
+args = parser.parse_args()
+
+host = args.host
+port = args.port
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((host, port))
+    s.listen()
+
+    print(f"Server streaming on {host}:{port}")
+
+    while True:
+        conn, addr = s.accept()
+        print(f"Client connected from {addr}")
+
+        with conn:
+            try:
+                while True:
+                    num1 = random.uniform(0, 100)
+                    num2 = random.uniform(0, 100)
+                    message = struct.pack('dd', num1, num2)
+
+                    conn.sendall(message)
+                    time.sleep(0.01)
+            except (BrokenPipeError, ConnectionResetError, ConnectionAbortedError):
+                print(f"Client disconnected from {addr}")
