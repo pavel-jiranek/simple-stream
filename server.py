0e42ae67c297209c82b54f9bb1c06e465ce52eb3
Pavel Jiranek <pavel.jiranek@raidyn.ai>
2025-04-25 14:47:52 +0200

    Adjust socket options

--- server.py
+++ server.py
@@ -18,6 +18,7 @@ host = args.host
 port = args.port
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((host, port))
     s.listen()
 
