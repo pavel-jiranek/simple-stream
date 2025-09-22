657c7bbb33b2c6d7e10e4d4597d3b35afbd61d72
Pavel Jiranek <pavel.jiranek@raidyn.ai>
2025-09-22 13:53:33 +0200

    Add option to write to output file

--- client.py
+++ client.py
@@ -3,6 +3,7 @@ A simple TCP client that receives a fixed amount of real floating point numbers 
 """
 
 import argparse
+import pathlib
 import socket
 import struct
 import time
@@ -31,6 +32,13 @@ parser.add_argument(
     default=10,
     help="Expected size of the message (default: 500)",
 )
+parser.add_argument(
+    "--output-file",
+    type=str,
+    default=None,
+    help="File to write the received numbers to (default: none)",
+    required=False,
+)
 
 args = parser.parse_args()
 
@@ -38,26 +46,45 @@ host = args.host
 port = args.port
 period = args.period
 bufsize = args.size * struct.calcsize("<f")
+output_file = pathlib.Path(args.output_file) if args.output_file else None
+
 
 def receive_stream(s):
     overall_start_time = time.monotonic()
-    while True:
-        start_time = time.monotonic()
-        data = s.recv(bufsize)
-        if not data:
-            print("No data received")
-            break
+    file = None
+    if output_file:
+        file = output_file.open("w", encoding="utf-8")
 
-        unpacked = struct.iter_unpack("<f", data)
-        values = [item[0] for item in unpacked]
-        if len(values) <= 3:
-            formatted = ", ".join(["{:.3f}".format(num) for num in values])
-        else:
-            formatted = "{:.3f}, {:.3f},... {:.3f}".format(values[0], values[1], values[-1])
-        timestamp = time.monotonic() - overall_start_time
-        print("{:.0f}s: Received {} values: {}".format(timestamp, len(values), formatted))
+    try:
+        while True:
+            start_time = time.monotonic()
+            data = s.recv(bufsize)
+            if not data:
+                print("No data received")
+                break
 
-        time.sleep(max(0, period - (time.monotonic() - start_time)))
+            unpacked = struct.iter_unpack("<f", data)
+            values = [item[0] for item in unpacked]
+            if len(values) <= 3:
+                formatted = ", ".join(["{:.3f}".format(num) for num in values])
+            else:
+                formatted = "{:.3f}, {:.3f},... {:.3f}".format(
+                    values[0], values[1], values[-1]
+                )
+            timestamp = time.monotonic() - overall_start_time
+            print(
+                "{:.0f}s: Received {} values: {}".format(
+                    timestamp, len(values), formatted
+                )
+            )
+
+            if file:
+                file.writelines([f"{v}\n" for v in values])
+
+            time.sleep(max(0, period - (time.monotonic() - start_time)))
+    finally:
+        if file:
+            file.close()
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -65,4 +92,3 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((host, port))
 
     receive_stream(s)
-
